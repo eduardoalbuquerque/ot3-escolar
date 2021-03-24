0b48c7ff0c9c0dd28afa40a0144cd9b9004bc33d
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2021-03-24 05:44:18 -0300

    first commit

---
+++ src/main/java/br/com/zup/ot3/controledeturma/ControledeturmaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.ot3.controledeturma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControledeturmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControledeturmaApplication.class, args);
+    }
+
+}
